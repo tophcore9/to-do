719b599f1a3125a67b4228e733b26d3c3bd34c72
Oleksii Paziura <o.paziura@imcagro.com.ua>
2025-02-20 18:48:25 +0200

    Add components for todo and add basic styles for it

--- task_3.6/src/App.vue
+++ task_3.6/src/App.vue
@@ -1,11 +1,22 @@
 <template>
-    
+    <div class="container">
+        <h1 class="title">Todo list</h1>
+        <TodoList/>
+    </div>
 </template>
 
 <script setup lang="ts">
+    import {defineComponent} from "vue";
+    import TodoList from "@/components/TodoList.vue";
 
+    defineComponent({TodoList});
 </script>
 
 <style>
 @import 'bulma/css/bulma.css';
+
+.container {
+    padding: 10px;
+    max-width: 700px !important;
+}
 </style>
---
+++ task_3.6/src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div v-for="i in 3">
+        <div class="card card-body p-2 mb-5 theme-light">
+            <div>
+                Test task {{i}}
+            </div>
+            <div>
+                <button class="button mr-2 is-success has-text-light">
+                    &check;
+                </button>
+                <button class="button is-danger has-text-light">
+                    &cross;
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {defineComponent} from "vue";
+import TodoItem from "@/components/TodoItem.vue";
+
+defineComponent({
+    TodoItem
+});
+
+defineProps({
+    // items: {
+    //     type: TodoItem,
+    //
+    // }
+})
+</script>
+
+<style scoped>
+.card-body {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
