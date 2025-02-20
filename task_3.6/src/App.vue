de8f50805f5af88f22a16ba6956652bdd7ffdd58
Oleksii Paziura <o.paziura@imcagro.com.ua>
2025-02-20 19:08:57 +0200

    Add a field for adding new tasks

--- task_3.6/src/App.vue
+++ task_3.6/src/App.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="container">
-        <h1 class="title">Todo list</h1>
+    <div class="container theme-light">
+        <h1 class="title has-text-centered">Todo list</h1>
+        <div class="add-todo">
+            <input type="text" class="input mb-5 p-5" placeholder="Add a task">
+            <button class="button w-3 mb-5 p-3 is-success has-text-light">Add</button>
+        </div>
         <TodoList/>
     </div>
 </template>
@@ -18,5 +22,12 @@
 .container {
     padding: 10px;
     max-width: 700px !important;
+}
+
+.add-todo {
+    display: flex;
+    justify-content: stretch;
+    align-items: center;
+    gap: 5px;
 }
 </style>
--- task_3.6/src/components/TodoList.vue
+++ task_3.6/src/components/TodoList.vue
@@ -1,8 +1,8 @@
 <template>
-    <div v-for="i in 3">
-        <div class="card card-body p-2 mb-5 theme-light">
+    <div v-for="item in todos">
+        <div class="card card-body p-2 pl-5 mb-5">
             <div>
-                Test task {{i}}
+                {{item.content}}
             </div>
             <div>
                 <button class="button mr-2 is-success has-text-light">
@@ -17,8 +17,31 @@
 </template>
 
 <script setup lang="ts">
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
 import TodoItem from "@/components/TodoItem.vue";
+
+const todos = ref([
+    {
+        id: 1,
+        content: 'Feed the cat',
+        done: true
+    },
+    {
+        id: 2,
+        content: 'Feed yourself',
+        done: false
+    },
+    {
+        id: 3,
+        content: 'Take a shower',
+        done: false
+    },
+    {
+        id: 4,
+        content: 'Something important',
+        done: true
+    },
+])
 
 defineComponent({
     TodoItem
