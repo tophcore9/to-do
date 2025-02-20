263c233600732848cad7b4d5d5a23b1889597d49
Oleksii Paziura <o.paziura@imcagro.com.ua>
2025-02-20 19:33:25 +0200

    Add the possibility of adding new todo-items

--- task_3.6/src/App.vue
+++ task_3.6/src/App.vue
@@ -1,19 +1,53 @@
 <template>
     <div class="container theme-light">
         <h1 class="title has-text-centered">Todo list</h1>
-        <div class="add-todo">
-            <input type="text" class="input mb-5 p-5" placeholder="Add a task">
-            <button class="button w-3 mb-5 p-3 is-success has-text-light">Add</button>
-        </div>
-        <TodoList/>
+        <form @submit.prevent="addItem" class="add-todo mb-4">
+            <input v-model="currentAddValue" type="text" class="input mb-5 p-5" placeholder="Add a task">
+            <button class="button w-3 mb-5 p-3 is-success has-text-light" :disabled="!currentAddValue">Add</button>
+        </form>
+        <TodoList :items="todos"/>
     </div>
 </template>
 
 <script setup lang="ts">
-    import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
     import TodoList from "@/components/TodoList.vue";
 
     defineComponent({TodoList});
+
+    /* VARIABLES */
+    const todos = ref([
+        {
+            id: 1,
+            content: 'Feed the cat',
+            done: true
+        },
+        {
+            id: 2,
+            content: 'Feed yourself',
+            done: false
+        },
+        {
+            id: 3,
+            content: 'Take a shower',
+            done: false
+        },
+        {
+            id: 4,
+            content: 'Something important',
+            done: true
+        },
+    ]);
+    const currentAddValue = ref('');
+
+    /* METHODS */
+    const addItem = () => {
+        todos.value.push({
+            id: 1,
+            content: currentAddValue.value,
+            done: false
+        });
+    }
 </script>
 
 <style>
--- task_3.6/src/components/TodoList.vue
+++ task_3.6/src/components/TodoList.vue
@@ -1,9 +1,7 @@
 <template>
-    <div v-for="item in todos">
+    <div v-for="item in items">
         <div class="card card-body p-2 pl-5 mb-5">
-            <div>
-                {{item.content}}
-            </div>
+            <input type="text" class="input" v-model="item.content">
             <div>
                 <button class="button mr-2 is-success has-text-light">
                     &check;
@@ -20,38 +18,15 @@
 import {defineComponent, ref} from "vue";
 import TodoItem from "@/components/TodoItem.vue";
 
-const todos = ref([
-    {
-        id: 1,
-        content: 'Feed the cat',
-        done: true
-    },
-    {
-        id: 2,
-        content: 'Feed yourself',
-        done: false
-    },
-    {
-        id: 3,
-        content: 'Take a shower',
-        done: false
-    },
-    {
-        id: 4,
-        content: 'Something important',
-        done: true
-    },
-])
-
 defineComponent({
     TodoItem
 });
 
 defineProps({
-    // items: {
-    //     type: TodoItem,
-    //
-    // }
+    items: {
+        type: Object,
+        required: true
+    }
 })
 </script>
 
@@ -60,5 +35,11 @@ defineProps({
     display: flex;
     justify-content: space-between;
     align-items: center;
+}
+
+.input {
+    border: none;
+    outline: none;
+    width: fit-content;
 }
 </style>
